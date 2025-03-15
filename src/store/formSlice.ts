89a4949ef0a05649a5cc3b506938db3219802633
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-03-15 18:44:53 +0200

    feat: add store and redux

---
+++ src/store/formSlice.ts
@@ -0,0 +1,39 @@
+
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface FormData {
+  name: string;
+  age: number;
+  email: string;
+  password: string;
+  gender: string;
+  terms: boolean;
+  picture: string;
+  country: string;
+}
+
+interface FormState {
+  uncontrolledForm: FormData | null;
+  hookForm: FormData | null;
+}
+
+const initialState: FormState = {
+  uncontrolledForm: null,
+  hookForm: null,
+};
+
+const formSlice = createSlice({
+  name: 'form',
+  initialState,
+  reducers: {
+    setUncontrolledFormData(state, action: PayloadAction<FormData>) {
+      state.uncontrolledForm = action.payload;
+    },
+    setHookFormData(state, action: PayloadAction<FormData>) {
+      state.hookForm = action.payload;
+    },
+  },
+});
+
+export const { setUncontrolledFormData, setHookFormData } = formSlice.actions;
+export default formSlice.reducer;
