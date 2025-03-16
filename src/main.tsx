22bca3ad1b8671e9cabb9f9a0cc8626dc4d41594
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-03-17 00:28:11 +0200

    feat: add Main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './App';
 import './index.css';
-import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
-    </StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </StrictMode>,
   );
 } else {
   console.error('Root element not found');
---
+++ src/routes/Main.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import styles from "./Main.module.css";
+
+function Main() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.card}>
+        <h1 className={styles.title}>Forms Showcase</h1>
+        <p className={styles.description}>Please select a form type below:</p>
+
+        <nav className={styles.nav}>
+          <Link className={styles.btn} to="/uncontrolled-form">
+            Uncontrolled Form
+          </Link>
+          <Link className={styles.btn} to="/hook-form">
+            Hook Form
+          </Link>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
